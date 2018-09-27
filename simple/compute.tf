560825a1d27f7df908534cdce5ac53cb6a7f7801
Ben Lackey <ben.lackey@outlook.com>
2018-09-26 17:02:56 -0700

    change to node

--- simple/compute.tf
+++ simple/compute.tf
@@ -12,7 +12,7 @@ resource "oci_core_instance" "h2o" {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(format("%s\n%s\n",
       "#!/usr/bin/env bash",
-      file("../scripts/server.sh")
+      file("../scripts/node.sh")
     ))}"
   }
   count = "${var.h2o["node_count"]}"
