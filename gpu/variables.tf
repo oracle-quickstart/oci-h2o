107fa08df16c4dd66a2e1f64b90fb01b78c772c1
Oguz Pastirmaci <oguzp@outlook.com>
2019-02-13 14:02:29 -0800

    Update OS image to Oracle-Linux-7.6-Gen2-GPU-2019.01.17-0

--- gpu/variables.tf
+++ gpu/variables.tf
@@ -6,6 +6,7 @@
 
 # Required by the OCI Provider
 variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
@@ -34,13 +35,14 @@ variable "h2o" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 // https://docs.cloud.oracle.com/iaas/images/image/e4de7fce-d505-4c4c-a94a-0e9a72f3a259/
-// Oracle-Linux-7.5-Gen2-GPU-2018.08.14-0
+// Oracle-Linux-7.6-Gen2-GPU-2019.01.17-0
 variable "images" {
   type = "map"
   default = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaimxelxqd3s77m3wsu7argbasbp2mz35qei7xiu6gqb3uf57ywnaa"
-    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaapq5wdwd7y2c57iqi3vduixk7oxwduni5cbtjmeskocg5thrlqo5a"
-    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaai66gcuvoue5zblvogi7mvi7lomgxm6nat6mck7uumvmsz5gmpg7q"
-    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaao4bkg34ku4ok5pqr4i3dxlath267ge47yh46ctqfjjuigde3m5qa"
+    ca-toronto-1 = "ocid1.image.oc1.ca-toronto-1.aaaaaaaau2dbdv7pdz2ove442ixqfi2a4ngir7sjt3nqa25ykqjc4mw36qiq"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaasmqd4eownxal3jw3mg5p3kpg2vhg2wh2uix56eabvzdsnqzb27ma"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaakzhli25jz7qb7papgx7cn2ssmfq347s23uqfxwhimz7wx7ewti6q"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaalzupbu66pqfc2e57oxbyqkjzeygiiec4nhehfnq4acrkly2l3aya"
+    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaairsn23mmdyz6qa6payovc6fwadsd5wunwz6lxb3obqkixmj45oca"
   }
 }
