fb0e2e3ce8c14ea9b973975c0b3a8aaf45e67a49
Oguz Pastirmaci <oguzp@outlook.com>
2019-02-13 19:51:49 -0800

    Disable local firewall

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,11 +1,12 @@
-echo "Running node.sh"
+#!/bin/bash
 
+echo "Running node.sh"
 #######################################################
 ################# Turn Off the Firewall ###############
 #######################################################
-echo "Adding port 12345 to firewall"
-firewall-cmd --permanent --zone=public --add-port=12345/tcp
-firewall-cmd --reload
+echo "Turning off the Firewall..."
+service firewalld stop
+chkconfig firewalld off
 
 #######################################################
 ############### Install H2O Driverless AI #############
@@ -15,6 +16,7 @@ cd /
 curl -O https://s3.amazonaws.com/artifacts.h2o.ai/releases/ai/h2o/dai/rel-1.5.3-11/x86_64-centos7/dai-1.5.3-1.x86_64.rpm
 rpm -i dai-1.5.3-1.x86_64.rpm
 
+echo 0 > /proc/sys/vm/overcommit_memory
 mkdir -p /opt/h2oai/dai/home/.driverlessai/
 echo $key > /opt/h2oai/dai/home/.driverlessai/license.sig
 
