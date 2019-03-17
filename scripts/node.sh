a17a293d276eb3a2c8474186023a0f4aa6f044bf
Ben Lackey <ben.lackey@outlook.com>
2019-03-17 15:43:27 -0700

    new version

--- scripts/node.sh
+++ scripts/node.sh
@@ -13,7 +13,7 @@ chkconfig firewalld off
 #######################################################
 echo "Installing H2O Driverless AI..."
 cd /
-curl -O https://s3.amazonaws.com/artifacts.h2o.ai/releases/ai/h2o/dai/rel-1.5.3-11/x86_64-centos7/dai-1.5.3-1.x86_64.rpm
+curl -O https://s3.amazonaws.com/artifacts.h2o.ai/releases/ai/h2o/dai/rel-1.5.4-65/x86_64-centos7/dai-1.5.4-1.x86_64.rpm
 rpm -i dai-1.5.3-1.x86_64.rpm
 
 echo 0 > /proc/sys/vm/overcommit_memory
