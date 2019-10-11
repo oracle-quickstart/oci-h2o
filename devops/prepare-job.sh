92081732ca258052a69316ab07b4d190aa8f8f32
Radu Dobrinescu <dobrinescu@yahoo.com>
2019-10-11 17:36:32 +0300

    test packer job

--- devops/prepare-job.sh
+++ devops/prepare-job.sh
@@ -34,3 +34,5 @@ export VER="1.4.1"
 wget https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
 unzip packer_${VER}_linux_amd64.zip
 sudo mv packer /usr/local/bin
+
+rm -rf terraform_0.12.10_linux_amd64.zip packer_${VER}_linux_amd64.zip
