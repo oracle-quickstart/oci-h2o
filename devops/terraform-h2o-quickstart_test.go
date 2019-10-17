a6f31584b922bdb9495314ce9ef270719da5b103
Radu Dobrinescu <dobrinescu@yahoo.com>
2019-10-17 13:33:53 +0300

    dsfsdf

--- devops/terraform-h2o-quickstart_test.go
+++ devops/terraform-h2o-quickstart_test.go
@@ -59,7 +59,7 @@ func TestQuickstartTerraformCode(t *testing.T) {
 	//driverlessAiUrl := terraform.Output(t, terraformOptions, "Driverless_AI_URL") 
 
 	ImageName := os.Getenv("GITHUB_REPOSITORY")
-	PackerDir:= fmt.Sprint("%s/../devops", os.Getenv("TF_ACTION_WORKING_DIR")),
+	PackerDir:= fmt.Sprint("%s/../devops", os.Getenv("TF_ACTION_WORKING_DIR"))
 	shellCommand := shell.Command{
 			Command: fmt.Sprint("/usr/bin/jq '.builders[].base_image_ocid |= %s | .builders[].image_name |= %s' %s/marketplace-image.json", ImageOCID, ImageName, PackerDir),
 	}
