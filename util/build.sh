90bfdb8667c7f86b18c30527556905fcde6a1c4d
Collin Poczatek <collin.poczatek@gmail.com>
2020-04-13 08:38:46 -0700

    clean to reflect reorg

--- util/build.sh
+++ util/build.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
 # Builds mkpl .zip for ORM. Uses local copy of existing TF.
-# Replaces: variables.tf
-# Adds: mkpl-schema.yaml, image_subscription.tf
 # Output: $out_file
 
 out_file="mkpl.zip"
-schema="mkpl-schema.yaml"
-variables="mkpl-variables.tf"
 
 echo "TEST cleanup"
 rm -rf ./tmp_package
@@ -21,15 +17,11 @@ cp -v ../terraform/*.tf ./tmp_package
 echo "Copying schema.yaml to tmp dir...."
 cp -v ../terraform/schema.yaml ./tmp_package
 echo "Copying script directory to tmp dir...."
-cp -rv ../scripts ./tmp_package
+cp -rv ./scripts ./tmp_package
 
 
 echo "Removing provider.tf...."
 rm ./tmp_package/provider.tf
-
-# Required path change since schema.yaml forces working directory to be
-# root of .zip
-sed -i '' "s:file(\"../scripts/node.sh\"):file(\"./scripts/node.sh\"):g" ./tmp_package/compute.tf
 
 # Add latest git log entry
 git log -n 1 > tmp_package/git.log
