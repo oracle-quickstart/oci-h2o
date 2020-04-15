ec090cf8ab47c753b4880c7b45ff824d0a302186
dbcopas <douglas.copas@gmail.com>
2020-04-15 18:05:01 +0200

    Revert "nit picking"
    This reverts commit 20389a455da290a09ac023a6658e2573a27da3c6.

--- marketplace/build.sh
+++ marketplace/build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# Builds a marketplace.zip for ORM.  This uses a local copy of existing TF.
+# Builds marketplace.zip for ORM. Uses local copy of existing TF.
+# Output: $out_file
 
 out_file="marketplace.zip"
 
-echo "Cleaning up...."
+echo "TEST cleanup"
 rm -rf ./tmp_package
-rm ${out_file}
+rm $out_file
 
 echo "Creating tmp dir...."
 mkdir ./tmp_package
@@ -26,9 +27,9 @@ git log -n 1 > tmp_package/git.log
 
 echo "Creating $out_file ...."
 cd tmp_package
-zip -r ${out_file} *
+zip -r $out_file *
 cd ..
-mv tmp_package/${out_file} ./
+mv tmp_package/$out_file ./
 
 echo "Deleting tmp dir...."
 rm -rf ./tmp_package
