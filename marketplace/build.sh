38814c7412e3c01654a2015cc1c0503b65e79f9f
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2020-04-16 10:44:38 +0200

    use new dir structure in build script

--- marketplace/build.sh
+++ marketplace/build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Builds mkpl .zip for ORM. Uses local copy of existing TF.
-# Output: $out_file
+out_file="marketplace.zip"
 
-out_file="mkpl.zip"
-
-echo "TEST cleanup"
+echo "Cleaning up...."
 rm -rf ./tmp_package
-rm $out_file
+rm ${out_file}
 
 # set after cleanup, since failure of that rm is ok
 set -euo
@@ -16,12 +13,11 @@ echo "Creating tmp dir...."
 mkdir ./tmp_package
 
 echo "Copying .tf files to tmp dir...."
-cp -v ../terraform/*.tf ./tmp_package
+cp -v ../*.tf ./tmp_package
 echo "Copying schema.yaml to tmp dir...."
-cp -v ../terraform/schema.yaml ./tmp_package
+cp -v ./schema.yaml ./tmp_package
 echo "Copying script directory to tmp dir...."
-cp -rv ./scripts ./tmp_package
-
+cp -rv ../scripts ./tmp_package
 
 echo "Removing provider.tf...."
 rm ./tmp_package/provider.tf
@@ -31,9 +27,9 @@ git log -n 1 > tmp_package/git.log
 
 echo "Creating $out_file ...."
 cd tmp_package
-zip -r $out_file *
+zip -r ${out_file} *
 cd ..
-mv tmp_package/$out_file ./
+mv tmp_package/${out_file} ./
 
 echo "Deleting tmp dir...."
 rm -rf ./tmp_package
